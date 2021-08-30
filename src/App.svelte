7b6f9487156ac73544ba7cc7186012e1d1ae9eb6
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-08-30 11:01:04 +0700

    Production

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,57 @@
 <script lang="ts">
-	export let name: string;
+    //const lucky: string = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    let luck: boolean = false;
+
+    // async function getDoujin(id: string) {
+    //     let url: string = "https://nhentai.net/g/" + id + "/";
+    //     const response: Response = await fetch(url, { mode: "no-cors" });
+    //     console.log(response);
+    // }
+
+    function getLucky() {
+        //window.location.replace(lucky);
+        luck = true;
+    }
+
+    //getDoujin("177013");
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>Hello Gaymrs!</h1>
+    <h2>This website is made for Nathan</h2>
+    {#if luck}
+        <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+        />
+    {:else}
+        <button on:click={getLucky}>Click Here</button>
+    {/if}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
