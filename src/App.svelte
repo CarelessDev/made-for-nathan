b2aaf215828f7cbfbd0b653b7a21fc5b527bc0db
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-09-06 20:02:16 +0700

    Inescapable Rick Roll

--- src/App.svelte
+++ src/App.svelte
@@ -10,23 +10,23 @@
     <h1>Hello Gaymrs!</h1>
     <h2>This website is made for Nathan</h2>
     {#if luck}
-        <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-        />
-        <br />
-        <a href="https://nhentai.net/g/177013/"
-            ><img
-                src="https://www.nme.com/wp-content/uploads/2021/07/RickAstley2021.jpg"
-                alt="Rick Astley"
-                height="200px"
-            /></a
-        >
+        <div id="CursedContainer">
+            <a href="https://nhentai.net/g/177013/" target="_blank">
+                <div id="UnholyBox" /></a
+            >
+            <iframe
+                id="RickAstley"
+                src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            />
+        </div>
+        <h2>
+            Hello Comrade, You can't stop this Rick Roll.<br />
+            Trust me, <span class="red">DON'T CLICK IT.</span> You've been warned.
+        </h2>
     {:else}
         <button on:click={getLucky}>Click Here</button>
     {/if}
@@ -45,6 +45,33 @@
         text-transform: uppercase;
         font-size: 4em;
         font-weight: 100;
+    }
+
+    #CursedContainer,
+    #UnholyBox,
+    #RickAstley {
+        height: 315px;
+        width: 560px;
+    }
+
+    #CursedContainer {
+        margin: 0 auto;
+    }
+
+    #UnholyBox {
+        margin: 0 auto;
+        float: left;
+        /*background-color: rgba(255, 0, 0, 0.5);*/
+        position: absolute;
+    }
+
+    #RickAstley {
+        margin: 0 auto;
+        float: left;
+    }
+
+    span.red {
+        color: red;
     }
 
     @media (min-width: 640px) {
